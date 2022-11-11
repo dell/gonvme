e2ea406d320306c03ef62b225ed1b67839be2119
Konduri Satya Narayana <109734493+satyakonduri@users.noreply.github.com>
2022-11-11 12:16:10 +0530

    NVME_Utils: Update Parser to use json parser (#20)

--- gonvme_utils.go
+++ gonvme_utils.go
@@ -1,56 +1,56 @@
 package gonvme
 
 import (
+	"encoding/json"
+	log "github.com/sirupsen/logrus"
 	"regexp"
 	"strings"
 )
 
 type sessionParser struct{}
 
+// Single subsystem object
+type subsystems struct {
+	Name  string
+	NQN   string
+	Paths []map[string]string `json:"Paths"`
+}
+
+// Response of subsystems.
+type Response struct {
+	Subsys []subsystems `json:"Subsystems"`
+}
+
 func (sp *sessionParser) Parse(data []byte) []NVMESession {
 	str := string(data)
-	lines := strings.Split(str, "\n")
-
 	var result []NVMESession
-	var curSession *NVMESession
-	var Target string
-	var Name string
-
-	for _, line := range lines {
-		line = strings.ReplaceAll(strings.TrimSpace(line), ",", "")
-
-		switch {
-		case regexp.MustCompile("^\"NQN\"").Match([]byte(line)):
-			Target = strings.ReplaceAll(strings.Fields(line)[2], "\"", "")
-
-		case regexp.MustCompile("^\"Name\"").Match([]byte(line)):
-			Name = strings.ReplaceAll(strings.Fields(line)[2], "\"", "")
-
-		case regexp.MustCompile("^\"Transport\"").Match([]byte(line)):
-			session := NVMESession{}
-			session.Target = Target
-			session.Name = Name
-			session.NVMETransportName = NVMETransportName(strings.ReplaceAll(strings.Fields(line)[2], "\"", ""))
-			if curSession != nil {
-				result = append(result, *curSession)
-			}
-			curSession = &session
-
-		case regexp.MustCompile("^\"Address\"").Match([]byte(line)):
-			targetIP := strings.Split(strings.Fields(line)[2], "=")[1]
-			if strings.Split(strings.Fields(line)[3], "=")[0] == "host_traddr" {
-				curSession.Portal = targetIP
-			} else {
-				targetPortal := strings.ReplaceAll(strings.Split(strings.Fields(line)[3], "=")[1], "\"", "")
-				curSession.Portal = targetIP + ":" + targetPortal
-			}
-
-		case regexp.MustCompile("^\"State\"").Match([]byte(line)):
-			curSession.NVMESessionState = NVMESessionState(strings.ReplaceAll(strings.Fields(line)[2], "\"", ""))
-		}
+	var response Response
+	err := json.Unmarshal([]byte(str), &response)
+	if err != nil {
+		log.Error("JSON-encoded parsing error: ", err.Error())
+		return result
 	}
-	if curSession != nil {
-		result = append(result, *curSession)
+	for _, system := range response.Subsys {
+		session := NVMESession{}
+		session.Target = system.NQN
+		reAdd := `(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}`
+		re := regexp.MustCompilePOSIX(reAdd)
+		for _, path := range system.Paths {
+			session.Name = path["Name"]
+			session.NVMETransportName = NVMETransportName(path["Transport"])
+			if path["Transport"] == NVMeTransportTypeFC {
+				session.Portal = strings.Split(strings.Fields(path["Address"])[0], "=")[1]
+			} else if path["Transport"] == NVMeTransportTypeTCP {
+				if re.MatchString(path["Address"]) {
+					session.Portal = re.FindString(path["Address"]) + ":" + strings.ReplaceAll(strings.Split(strings.Fields(path["Address"])[1], "=")[1], "\"", "")
+				}
+			} else {
+				continue
+			}
+			session.NVMESessionState = NVMESessionState(path["State"])
+			result = append(result, session)
+
+		}
 	}
 	return result
 }
